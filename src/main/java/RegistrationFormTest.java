0bebd260df0c580b0f5ef17ea794b4cb0876f775
Uladzislau Liasnichy <51000407+Lesnichi@users.noreply.github.com>
2021-01-27 18:33:25 +0300

    Corrected 3 autotests and added 1 new autotest

--- src/main/java/RegistrationFormTest.java
+++ src/main/java/RegistrationFormTest.java
@@ -1,4 +1,5 @@
 
+import com.codeborne.selenide.Selenide;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -8,37 +9,52 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class RegistrationFormTest {
 
+    public static final String URL = "https://www.sharelane.com";
+    public static final String URL_ZIP_CODE_FORM = "https://www.sharelane.com/cgi-bin/register.py";
+
     @Test
-    public void ZipCodeTest() {
-        open("https://www.sharelane.com");
+    public void CorrectZipCodeTest() {
+        open(URL);
         $(By.xpath("//*[text()='ENTER']")).click();
-        open("https://www.sharelane.com/cgi-bin/register.py");
+        open(URL_ZIP_CODE_FORM);
         $(By.name("zip_code")).setValue("111").pressEnter();
-        By result = By.cssSelector(".error_message");
-        Assert.assertEquals(result, By.cssSelector(".error_message"));
+        String result = $(By.cssSelector(".error_message")).getText();
+        Assert.assertEquals(result, "Oops, error on page. ZIP code should have 5 digits");
+        Selenide.closeWebDriver();
+    }
+
+    @Test
+    public void InvalidZipCodeTest() {
+        open(URL);
+        $(By.xpath("//*[text()='ENTER']")).click();
+        open(URL_ZIP_CODE_FORM);
+        $(By.name("zip_code")).setValue("11111").pressEnter();
+        Assert.assertTrue($(By.name("first_name")).setValue("Vlad").isDisplayed());
+        Selenide.closeWebDriver();
     }
 
     @Test
     public void RegistrationFormTest1() {
-        open("https://www.sharelane.com");
+        open(URL);
         $(By.xpath("//*[text()='ENTER']")).click();
-        open("https://www.sharelane.com/cgi-bin/register.py");
+        open(URL_ZIP_CODE_FORM);
         $(By.name("zip_code")).setValue("11111").pressEnter();
         $(By.name("first_name")).setValue("Vlad").pressTab();
         $(By.name("last_name")).setValue("Lesnichi").pressTab();
         $(By.name("email")).setValue("qwerty@q.com").pressTab();
         $(By.name("password1")).setValue("1111").pressTab();
-        $(By.name("password2")).setValue("1111");
+        $(By.name("password2")).setValue("111");
         $(By.cssSelector("[value=Register]")).click();
-        By result = By.cssSelector(".error_message");
-        Assert.assertEquals(result, By.cssSelector(".error_message"));
+        String result = $(By.cssSelector(".error_message")).getText();
+        Assert.assertEquals(result, "Oops, error on page. Some of your fields have invalid data or email was previously used");
+        Selenide.closeWebDriver();
     }
 
     @Test
     public void RegistrationFormTest2() {
-        open("https://www.sharelane.com");
+        open(URL);
         $(By.xpath("//*[text()='ENTER']")).click();
-        open("https://www.sharelane.com/cgi-bin/register.py");
+        open(URL_ZIP_CODE_FORM);
         $(By.name("zip_code")).setValue("11111").pressEnter();
         $(By.name("first_name")).setValue("Vlad").pressTab();
         $(By.name("last_name")).setValue("Lesnichi").pressTab();
@@ -46,7 +62,8 @@ public class RegistrationFormTest {
         $(By.name("password1")).setValue("1111").pressTab();
         $(By.name("password2")).setValue("1111");
         $(By.cssSelector("[value=Register]")).click();
-        By result = By.cssSelector(".confirmation_message");
-        Assert.assertEquals(result, By.cssSelector(".confirmation_message"));
+        String result = $(By.cssSelector(".confirmation_message")).getText();
+        Assert.assertEquals(result, "Account is created!");
+        Selenide.closeWebDriver();
     }
 }
